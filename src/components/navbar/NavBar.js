d6a7b03f5f8e8815cebc424392d9e4a3f838ae9d
maxi <maxi_rosanda@hotmail.com>
2021-01-06 16:28:50 -0300

    sexto

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,26 +1,22 @@
-
+import { Navbar } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { Nav} from 'react-bootstrap';
+import { Form} from 'react-bootstrap';
+import { FormControl} from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 function Barra() {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Maxi Ecommerce</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Electrodomésticos<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Computación<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Herramientas<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Bazar<span class="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+    return <>
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Maxi e-commerce</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    </>
   }
   
   export default Barra;
