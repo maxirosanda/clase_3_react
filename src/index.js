9c8a1485cf3ff8a5406c53d1081a0025757029aa
maxi <maxi_rosanda@hotmail.com>
2021-01-06 13:44:31 -0300

    segundo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Barra from './componentes/navbar/navbar';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
-    <Barra />
+   
     <App />
   </React.StrictMode>,
   document.getElementById('root')
